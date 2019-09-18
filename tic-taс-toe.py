21c0a15f988815812a0812b2eb7489f4dac34e4d
Artem Kajalainen <artem.kajalainen@gofore.com>
2019-09-18 11:43:51 +0300

    randomize everything

--- tic-taс-toe.py
+++ tic-taс-toe.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 board_state = { 1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' ' }
 
@@ -8,13 +9,10 @@ def check_row(index1,index2,index3):
     else:
         return False
 
-def check_win(board_state,debug=False):
+def check_win(board_state):
     # 123
     # 456
     # 789
-    if debug:
-        print(board_state)
-        print_board(board_state)
     return check_row(1,2,3) or \
         check_row(4,5,6) or \
         check_row(7,8,9) or \
@@ -39,28 +37,37 @@ def print_board(board_state):
 {:^3}|{:^3}|{:^3}
 '''.format(board_state[1],board_state[2],board_state[3],board_state[4],board_state[5],board_state[6],board_state[7],board_state[8],board_state[9]))
 
+def get_random_player(gamers):
+    return random.choice(gamers)
+
+def get_other_player(starting_player, gamers_list):
+    return gamers_list[gamers_list.index(starting_player)-1]
+    # arr = gamers_list
+    # arr.remove(starting_player)
+    # return arr[0]
+
+def get_second_sign(first_sign, signs):
+    return signs[signs.index(first_sign)-1]
+    # arr = signs
+    # arr.remove(first_sign)
+    # return arr[0]
+
+def init_players(player_1, player_2):
+    starting_player = get_random_player([player_1,player_2])
+    second_player = get_other_player(starting_player,[player_1,player_2])
+    signs = ['x','o']
+    sign = ''
+    while sign not in signs:
+        sign = input(f"{starting_player}, choose your sign, please ('x'|'o'): ")
+    return (starting_player, {starting_player: sign, second_player: get_second_sign(sign, signs)}, {sign: starting_player, get_second_sign(sign, signs): second_player })
+
 player_1 = input("Player one name: ")
 player_2 = input("Player two name: ")
 
-player_1_sign = ''
-player_2_sign = 'x'
+starting_player, gamers, gamers_by_sign = init_players(player_1, player_2)
 
-while player_1_sign not in ('x','o'):
-    player_1_sign = input(f"{player_1}, choose your sign, please ('x'|'o'): ")
-    if player_1_sign == 'x': player_2_sign = 'o'
-
-# player_1 = 'ArtemK'
-# player_2 = 'Computer'
-# player_1_sign = 'x'
-# player_2_sign = 'o'
-
-gamers = { player_1_sign:player_1, player_2_sign:player_2 }
-
-player = player_1
-player_sign = player_1_sign
-
+player = starting_player
 # main loop
-step = 1
 while True:
     while not check_win(board_state) and ' ' in board_state.values():
         print_board(board_state)
@@ -70,19 +77,12 @@ while True:
             if 1 <= index <= 9 and board_state[index] != ' ':
                 print("Waat, that's cell already played")
                 index = 0
-        board_state[index] = player_sign
-        # change player
-        step += 1
-        if step % 2 == 0:
-            player = player_2
-            player_sign = player_2_sign
-        else:
-            player = player_1
-            player_sign = player_1_sign
+        board_state[index] = gamers[player]
+        player = get_other_player(player, list(gamers.keys()))
     print_board(board_state)
     result = check_win(board_state)
     if result:
-        print("Congrats {}, you won!".format(gamers[result]))
+        print("Congrats {}, you won!".format(gamers_by_sign[result]))
     else:
         print("That's a draw!")
     answer = ''
@@ -90,9 +90,7 @@ while True:
         answer = input("Play another one? (y|n): ")
     if answer == 'y':
         clear_board()
-        step = 1
-        player = player_1
-        player_sign = player_1_sign
+        player = get_random_player(list(gamers.keys()))
         continue
     else:
         print("Game over!")
